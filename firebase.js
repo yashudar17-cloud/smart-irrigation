8c914f33bc7021efcb53e43952c1e7fd3ccc0e93
yashudar17-cloud <yashudar17@gmail.com>
2026-04-14 20:55:44 +1100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,24 @@
-import { initializeApp } 
-from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBtsJ9rVqJWE93hPW7fYNQu25NVGX8FYuw",
-  authDomain: "smart-irrigation-system-e649a.firebaseapp.com",
-  databaseURL: "https://smart-irrigation-system-e649a-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "smart-irrigation-system-e649a",
-  storageBucket: "smart-irrigation-system-e649a.appspot.com",
-  messagingSenderId: "1001835662661",
-  appId: "1:1001835662661:web:dc866bb3f72ae50eadc22c",
-  measurementId: "G-GJKFJW2Z71"
-};
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBtsJ9rVqJWE93hPW7fYNQu25NVGX8FYuw",
+    authDomain: "smart-irrigation-system-e649a.firebaseapp.com",
+    databaseURL: "https://smart-irrigation-system-e649a-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "smart-irrigation-system-e649a",
+    storageBucket: "smart-irrigation-system-e649a.firebasestorage.app",
+    messagingSenderId: "1001835662661",
+    appId: "1:1001835662661:web:dc866bb3f72ae50eadc22c",
+    measurementId: "G-GJKFJW2Z71"
+  };
 
-const app = initializeApp(firebaseConfig);
-
-export default app;
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
